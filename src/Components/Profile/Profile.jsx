c1f8db5bb4d0dbd54cd7e0132ab12f2bb7173833
Camila CROSET <potycamila@gmail.com>
2025-07-18 17:21:03 +0200

    feat: finished Profile component layout and styling with final design

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import './Profile.scss'
+import React from "react";
+import "./Profile.scss";
+import profile_img from "../../assets/profile_img.svg";
 
 const Profile = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="profile">
+      <img src={profile_img} alt="" />
+      <h1><span>Camila Medeiros,</span> développeuse front-end</h1>
+      <p>
+        I am a frontend developer from California, USA with 10 years of
+        experience in multiple companies like Microsoft, Tesla and Apple.
+      </p>
+      <div className="profile__action">
+        <div className="profile__connect">connectez-vous avec moi</div>
+        <div className="profile__resume">Télécharger mon CV</div>
+      </div>
+    </section>
+  );
+};
 
-export default Profile
+export default Profile;
